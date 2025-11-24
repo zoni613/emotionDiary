b344833fc5a20e1987aef5fd257a76d8dee344a0
DESKTOP-SO89ORC\user <parksj980613@gmail.com>
2025-11-24 21:14:29 +0900

    feat: 이미지, 폰트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import New from './pages/New';
 import Diary from './pages/Diary';
 import Notfound from './pages/NotFound';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import {getEmotionImage} from  './util/get-emiotion-image';
 
 function App() {
   const nav = useNavigate();
@@ -13,6 +14,18 @@ function App() {
   }
   return (
     <>
+      <div>
+        {/* public 아래 두면 최적화가 일어나지 않아서
+            보통 이미지 최적화를 위해 assets 아래 둔다. */}
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+
+          {/* public 아래 위치할 경우 아래처럼 url로 불러온다. */}
+          {/* <img src="/emotion1.png" /> */}
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
